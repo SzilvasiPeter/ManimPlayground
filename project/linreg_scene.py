09aa072369c98d4a2ea41faa3f94dc95e0b95e9d
SzilvasiPeter <peti.szilvasi95@gmail.com>
2021-11-02 19:17:49 +0100

    Draw the best fit line with ordinary least square method.

--- project/linreg_scene.py
+++ project/linreg_scene.py
@@ -1,11 +1,17 @@
 from manim import *
+from manim.utils import deprecation
 import numpy as np
 
 # gradient descent
 class LinearRegression(Scene):
     def construct(self):
-        for i in range(0, 10):
-            self.add(Dot(np.random.randn(3)))
+        dot_list = []
+        coordinate_list = np.linspace(0, 3, 6)
+        for i in coordinate_list:
+            dot = Dot(np.random.randn(3))
+            # dot = Dot(np.array([i, 0.8*i, 0.0]))
+            dot_list.append(dot)
+            self.add(dot)
 
         # the location of the ticks depends on the x_range and y_range.
         grid = Axes(
@@ -23,9 +29,30 @@ class LinearRegression(Scene):
 
         # Draw a random Line.
         reg_line = Line(np.random.randn(3), np.random.randn(3))
-        reg_line.set_length(7)
+        reg_line.set_length(20)
         reg_line.set_color(RED)
+
+        # Draw the best fit Line.
+        x_all = 0.0
+        y_all = 0.0
+        for aDot in dot_list:
+            x_all += aDot.get_x()
+            y_all += aDot.get_y()
+
+        x_mean = x_all / len(dot_list)
+        y_mean = y_all / len(dot_list)
+
+        numerator = 0.0
+        denominator = 0.0
+        for aDot in dot_list:
+            numerator += ((aDot.get_x() - x_mean)*(aDot.get_y() - y_mean))
+            denominator += ((aDot.get_x() - x_mean)**2)
+
+        b_coeff = numerator / denominator
+        alpha_coeff = y_mean - (b_coeff*x_mean)
+
+        fitted_line = Line(np.array([0, 0+alpha_coeff, 0]), np.array([5, 5*b_coeff + alpha_coeff, 0]))
 
         self.add(grid, grid_labels)
         self.add(reg_line)
-
+        self.add(fitted_line)
